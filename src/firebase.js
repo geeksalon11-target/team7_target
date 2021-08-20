ebfb87406e774567b082644f13d9f5bfec56d5de
長瀬柊哉 <touya.nagase@ge-ne-sis.com>
2021-08-20 21:00:02 +0900

    初期設定しました！

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA_KJX0PAHZUZeqe0-3-AOH7LUkOUrsb_U",
+  authDomain: "team-project-c3631.firebaseapp.com",
+  projectId: "team-project-c3631",
+  storageBucket: "team-project-c3631.appspot.com",
+  messagingSenderId: "997474190551",
+  appId: "1:997474190551:web:922d440ac2d017cd5edc92",
+  measurementId: "G-VSTJCSG1TR",
+};
+
+firebase.initializeApp(firebaseConfig);
