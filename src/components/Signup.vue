cd46e122bcaa193be722b6d1030e394c48064642
Yosuke0831 <may.yosuke@ezweb.ne.jp>
2021-08-25 19:51:38 +0900

    メールアドレスとパスワードでのログインとほかの情報をfirestoreに保存する機能まで作った

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -13,6 +13,12 @@
       <tr>
         <td><input type="password" v-model="password" /></td>
       </tr>
+      <tr>
+        <th>名前：</th>
+      </tr>
+      <tr>
+        <td><input type="name" v-model="name" /></td>
+      </tr>
     </table>
 
     <button @click="signUp">登録</button>
@@ -28,12 +34,27 @@ export default {
       firebase
         .auth()
         .createUserWithEmailAndPassword(this.mailaddress, this.password)
-        .then
-        // 成功時の処理
-        ()
-        .catch
-        // エラー時の処理
-        ();
+        .then(
+          // 成功時の処理
+          (userCredential) => {
+            firebase
+              .firestore()
+              .collection("users")
+              .doc(userCredential.user.uid)
+              .set({
+                name: this.name,
+              });
+          }
+        )
+        .then(() => {
+          alert("成功！！");
+        })
+        .catch(
+          // エラー時の処理
+          () => {
+            alert("エラー");
+          }
+        );
     },
   },
 };
