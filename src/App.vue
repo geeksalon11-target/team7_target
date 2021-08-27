2a761d928a28e36b21e9a31c3873959f2edef6c0
toku-takuma <tokutaku1412@gmail.com>
2021-08-27 11:18:14 +0900

    キーワード検索機能を実装(検索結果はまだ未整形)

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div id="app">
+    <div><Coinfo /></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import Coinfo from "@/components/Coinfo.vue"
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Coinfo,
   },
-};
+}
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif, ;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: right;
+  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/Coinfo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="Coinfo">
+    <h1>企業情報検索サイト</h1>
+      <div>
+        検索ワード<input type="text" v-model="keywords" placeholder="検索キーワードを入力"/>
+        <button v-on:click="getCompanyNames()">企業を検索する</button>
+        <p>{{json}}</p>
+      </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      keywords:"",
+      json:""
+    }
+  },
+  methods: {
+    getCompanyNames: function () {
+        let url="https://u10sme-api.smrj.go.jp/v1/corporations.json?keywords=" + this.keywords;
+        axios
+        .get(url)
+        .then(response => {
+        this.json = response["data"]
+        });
+    }
+}
+}
+</script>
