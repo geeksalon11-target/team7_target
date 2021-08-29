997adf25d643fc1b23a0d11766a9ef3fd456a6e8
toku-takuma <tokutaku1412@gmail.com>
2021-08-29 14:33:30 +0900

    業種と地域の表示機能を追加

--- src/components/Coinfo.vue
+++ src/components/Coinfo.vue
@@ -8,14 +8,35 @@
         <!-- <ul v-for="corporation in corporations" v-bind:key="corporation">
           <h3>{{corporation.name}}</h3>
         </ul> -->
-        <ul>
+        <!-- <ul>
           <li v-for="(corporation, key) in corporations" :key="key">
             <a v-bind:href="corporation.url" target="_blank">
               {{corporation.name}}
             </a>
+          <div v-for="industry in industries" v-bind:key="industry">
+            <a>{{industry.name}}</a>
+          </div>
+          <div v-for="area in areas" v-bind:key="area">
+            <a>{{area.name}}</a>
+          </div>
           </li>
         </ul>
       </div>
+  </div> -->
+      <div v-for="corporation in corporations" v-bind:key="corporation">
+        <h3>
+          <router-link to="/CPage" v-on:click="toCPage(corporation.id)">{{
+            corporation.name
+          }}</router-link>
+        </h3>
+        <div v-for="serviceCategory in corporation.serviceCategories" v-bind:key="serviceCategory">
+          <div v-for="serviceKind in serviceCategory.serviceKinds" v-bind:key="serviceKind">
+            <p>＜{{ serviceKind.name }}＞</p>
+          </div>
+        </div>
+        <p>{{ corporation.location.address }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -26,8 +47,21 @@ export default {
     return {
       keywords:"",
       json:"",
-      corporations:[]
+      corporations:[],
+      industries:[],
+      areas:[]
     }
+  },
+  mounted() {
+    // 職種&地域検索用APIの取得
+    this.axios
+      .get("https://u10sme-api.smrj.go.jp/v1/serviceCategories.json")
+      .then((response) => (this.industries = response.data.serviceCategories))
+      .catch((error) => console.log(error));
+    this.axios
+      .get("https://u10sme-api.smrj.go.jp/v1/areas.json")
+      .then((response) => (this.areas = response.data.areas))
+      .catch((error) => console.log(error));
   },
   methods: {
     getCompanyNames: function () {
@@ -37,7 +71,19 @@ export default {
         .then(response => {
         this.corporations = response.data.corporations
         console.log(response)
-        });
+        })
+        // axios
+        // .get("https://u10sme-api.smrj.go.jp/v1/serviceCategories.json")
+        // .then(response => {
+        // this.industries = response.data.serviceCategories
+        // console.log(response);        
+        // });
+        // axios
+        // .get("https://u10sme-api.smrj.go.jp/v1/corporations.json?prefectures=" + this.keywords)
+        // .then(response => {
+        // this.areas = response.data.prefectures
+        // console.log(response);        
+        // })
     }
 }
 }
