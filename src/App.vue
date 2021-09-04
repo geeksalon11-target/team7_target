f99138654fc6bdeb8fa486ed228ec6bc9ef8ed29
green-penguin4 <unemotom2021@gmail.com>
2021-09-04 22:30:46 +0900

    デフォルトcssの解除

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <Navbar />
     <router-view />
   </div>
 </template>
 
-<script></script>
+<script>
+import Navbar from "./components/NavBar.vue";
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
+} */
 
-#nav {
-  padding: 30px;
+/* 初期設定解除 */
+ul {
+  list-style: none;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+body,
+ul {
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="NavBar">
+    <ul id="nav">
+      <li class="nav__content">
+        <router-link to="/">中小企業検索</router-link>
+      </li>
+      <li class="nav__content"><router-link to="">マイページ</router-link></li>
+      <li class="nav__content"><Signout /></li>
+    </ul>
+  </div>
+</template>
+<script>
+import Signout from "../components/Signout";
+export default {
+  components: {
+    Signout,
+  },
+};
+</script>
+
+<style scoped>
+#nav {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+.nav__content {
+  width: 200px;
+  padding: 1em 0;
+  font-size: 18px;
+  text-align: center;
+}
+</style>
