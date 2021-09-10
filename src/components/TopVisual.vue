be8db286f6588a911d282caaf7bdc11a2216e6aa
green-penguin4 <unemotom2021@gmail.com>
2021-09-10 17:05:21 +0900

    一旦コミット

--- src/components/TopVisual.vue
+++ src/components/TopVisual.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="top_visual">
-    <div class="site_title"><h1>中小企業情報検索</h1></div>
-    <div class="site_about"><p>サイト説明又はキャッチコピー</p></div>
+    <div class="site_title">
+      <h1>中小企業情報検索</h1>
+
+      <p>全国の中小企業を探せるサイト</p>
+    </div>
   </div>
 </template>
